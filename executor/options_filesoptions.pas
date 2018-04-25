1f723ad23df1b1c48ca48addc1927958d1e822c0
torstenchr <github.com@bondechristiansen.dk>
2018-04-25 10:10:25 +0000

    * FORGOT to add new unit... Fail

---
+++ executor/options_filesoptions.pas
@@ -0,0 +1,54 @@
+unit options_filesoptions;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, options_hashmap, Graphics;
+
+type
+
+  { TCustomFontOption }
+
+  { TCustomFileOption }
+
+  TCustomFileOption = class(TSetOption)
+  protected
+    procedure DoOptionError(Const S: String);
+  end;
+
+  { TFolderOption }
+
+  TFolderOption = class(TCustomFileOption)
+  protected
+    procedure SetValue(AValue: UTF8String); override;
+  end;
+
+
+implementation
+
+uses
+  LazFileUtils;
+
+{ TFolderOption }
+
+procedure TFolderOption.SetValue(AValue: UTF8String);
+begin
+   if (not DirectoryExistsUTF8(AValue)) then
+     DoOptionError('Folder: ' + AValue + LineEnding +
+                   'Does not exist!'
+     );
+
+  inherited SetValue(AValue);
+end;
+
+{ TCustomFileOption }
+
+procedure TCustomFileOption.DoOptionError(const S: String);
+begin
+  raise ESetOption.Create(S);
+end;
+
+end.
+
