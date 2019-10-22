3ce681ab7cca0e6f36390ed969416ad78dec87df
torstenchr <github.com@bondechristiansen.dk>
2019-10-22 17:06:52 +0000

    * Reverted changes to ast in r1075

--- ast.pas
+++ ast.pas
@@ -4709,7 +4709,6 @@ begin
     Exit;
 
   case EV.VarType of
-    evtField,
     evtGlobal,
     evtResultConst:
         // A global or result can be accepted as either Value or Object.
@@ -4718,6 +4717,7 @@ begin
     evtDataset,
     evtValuelabel,
     evtGlobalVector,
+    evtField,
     evtResultVector,
     evtResultMatrix:
       begin
@@ -4726,6 +4726,7 @@ begin
         Result := (evfAsObject in TypesAndFlags.Flags);
 
         if (not Result) then
+ { TODO -oJamie : Why not just recast this as having the default index? }
         begin
           DoTypeCheckError('Identifier "' + Ident + '" must have an index', TypeChecker);
           Exit;
