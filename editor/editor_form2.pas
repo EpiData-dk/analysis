112e88628ba70e5840cf651f7b0fe4464599b513
torstenchr <github.com@bondechristiansen.dk>
2020-02-19 21:17:16 +0000

    * Further work on new Editor that also supports mac

--- editor/editor_form2.pas
+++ editor/editor_form2.pas
@@ -28,11 +28,8 @@ type
     PageControl: TPageControl;
     StatusBar: TStatusBar;
     procedure CreateComponents;
-
     procedure CreateMainMenu;
-
     procedure CreateStatusBar;
-
   public
     constructor Create(AOwner: TComponent); override;
   end;
@@ -84,29 +81,69 @@ begin
   // File:
   TopMenuItem := TMenuItem.Create(Self);
   TopMenuItem.Caption := '&File';
-  TopMenuItem.Add(CreateActionAndMenuItem('&New',     @NewActionExecute,  ShortCut(VK_N, [ssCtrl])));
-  TopMenuItem.Add(CreateActionAndMenuItem('&Open...', @NoneActionExecute, ShortCut(VK_O, [ssCtrl])));
+  TopMenuItem.Add(CreateActionAndMenuItem('&New',        @NewActionExecute,  ShortCut(VK_N, [ssCtrl])));
+  TopMenuItem.Add(CreateActionAndMenuItem('&Open...',    @NoneActionExecute, ShortCut(VK_O, [ssCtrl])));
   // Open Recent....
-  TopMenuItem.Add(CreateActionAndMenuItem('&Save',    @NoneActionExecute, ShortCut(VK_S, [ssCtrl])));
+  TopMenuItem.Add(CreateActionAndMenuItem('&Save',       @NoneActionExecute, ShortCut(VK_S, [ssCtrl])));
   TopMenuItem.Add(CreateActionAndMenuItem('Save &As...', @NoneActionExecute, ShortCut(VK_S, [ssCtrl, ssShift])));
   TopMenuItem.Add(CreateDivider());
-  TopMenuItem.Add(CreateActionAndMenuItem('&Font...', @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('&Font...',    @NoneActionExecute, 0));
   TopMenuItem.Add(CreateDivider());
-  TopMenuItem.Add(CreateActionAndMenuItem('&Close Tab', @NoneActionExecute, ShortCut(VK_W, [ssCtrl])));
-  TopMenuItem.Add(CreateActionAndMenuItem('&Quit',      @NoneActionExecute, ShortCut(VK_Q, [ssShift])));
+  TopMenuItem.Add(CreateActionAndMenuItem('&Close Tab',  @NoneActionExecute, ShortCut(VK_W, [ssCtrl])));
+  TopMenuItem.Add(CreateActionAndMenuItem('&Quit',       @NoneActionExecute, ShortCut(VK_Q, [ssShift])));
   MainMenu.Items.Add(TopMenuItem);
 
   // Edit
   TopMenuItem := TMenuItem.Create(Self);
   TopMenuItem.Caption := 'Edit';
-  TopMenuItem.Add(CreateActionAndMenuItem('Insert History',     @NoneActionExecute, ShortCut(VK_I, [ssAlt])));
-  TopMenuItem.Add(CreateActionAndMenuItem('Insert Set Options', @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Insert History',               @NoneActionExecute, ShortCut(VK_I, [ssAlt])));
+  TopMenuItem.Add(CreateActionAndMenuItem('Insert Set Options',           @NoneActionExecute, 0));
   TopMenuItem.Add(CreateDivider());
-  TopMenuItem.Add(CreateActionAndMenuItem('Cut',   @NoneActionExecute, 0));
-  TopMenuItem.Add(CreateActionAndMenuItem('Copy',  @NoneActionExecute, 0));
-  TopMenuItem.Add(CreateActionAndMenuItem('Paste', @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Cut',                          @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Copy',                         @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Paste',                        @NoneActionExecute, 0));
   TopMenuItem.Add(CreateDivider());
   TopMenuItem.Add(CreateActionAndMenuItem('Preferences... (startup.pgm)', @NoneActionExecute, 0));
+  MainMenu.Items.Add(TopMenuItem);
+
+  // Search
+  TopMenuItem := TMenuItem.Create(Self);
+  TopMenuItem.Caption := '&Search';
+  TopMenuItem.Add(CreateActionAndMenuItem('Find...',       @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Find Next',     @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Find Previous', @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Replace...',    @NoneActionExecute, 0));
+  MainMenu.Items.Add(TopMenuItem);
+
+  // Run
+  TopMenuItem := TMenuItem.Create(Self);
+  TopMenuItem.Caption := '&Run';
+  TopMenuItem.Add(CreateActionAndMenuItem('Run Selected', @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Run All',      @NoneActionExecute, 0));
+  MainMenu.Items.Add(TopMenuItem);
+
+  // Window
+  TopMenuItem := TMenuItem.Create(Self);
+  TopMenuItem.Caption := '&Window';
+  TopMenuItem.Add(CreateActionAndMenuItem('Show Command Tree', @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Variables',         @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Command Prompt',    @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Editor',            @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Browse',            @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('History',           @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Dataset List',      @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateDivider());
+  TopMenuItem.Add(CreateActionAndMenuItem('Default Windowing', @NoneActionExecute, 0));
+  MainMenu.Items.Add(TopMenuItem);
+
+  // Help
+  TopMenuItem := TMenuItem.Create(Self);
+  TopMenuItem.Caption := '&Help';
+  TopMenuItem.Add(CreateActionAndMenuItem('Tutorials (Local)',        @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Tutorials (EpiData Wiki)', @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateActionAndMenuItem('Tutorials On Web',         @NoneActionExecute, 0));
+  TopMenuItem.Add(CreateDivider());
+  TopMenuItem.Add(CreateActionAndMenuItem('Commands Overview',        @NoneActionExecute, 0));
   MainMenu.Items.Add(TopMenuItem);
 
   Self.Menu := MainMenu;
@@ -127,6 +164,8 @@ begin
 
   Editor := EditorPage.Editor;
   Editor.Parent := Sheet;
+
+  PageControl.ActivePage := Sheet;
 
   EndFormUpdate;
 end;
--- editor/editor_page.pas
+++ editor/editor_page.pas
@@ -12,11 +12,11 @@ type
   { TEditorPage }
 
   TEditorPage = class
-
   private
     FEditor: TSynEdit;
     FFileName: UTF8String;
     function GetCaption: UTF8String;
+    procedure NoneExecuteAction(Sender: TObject);
     procedure SetFileName(AValue: UTF8String);
   public
     constructor Create;
@@ -29,7 +29,7 @@ type
 implementation
 
 uses
-  Controls, LazFileUtils;
+  Controls, LazFileUtils, editor_pgm_highlighter, Menus, Dialogs, ActnList;
 
 { TEditorPage }
 
@@ -50,16 +50,57 @@ begin
     Result := Result + '*';
 end;
 
+procedure TEditorPage.NoneExecuteAction(Sender: TObject);
+begin
+  ShowMessage('Action not implemented yet');
+end;
+
 constructor TEditorPage.Create;
+var
+  PGMHighlighter: TPGMHighLighter;
+  PopupMenu: TPopupMenu;
+
+  function CreateActionAndMenuItem(Caption: TCaption; OnExecute: TNotifyEvent; ShortCut: TShortCut): TMenuItem;
+  var
+    TheAction: TAction;
+  begin
+    TheAction := TAction.Create(FEditor);
+    TheAction.Caption := Caption;
+    TheAction.OnExecute := OnExecute;
+    TheAction.ShortCut := ShortCut;
+
+    Result := TMenuItem.Create(FEditor);
+    Result.Action := TheAction;
+  end;
+
+  function CreateDivider(): TMenuItem;
+  begin
+    Result := TMenuItem.Create(FEditor);
+    Result.Caption := '-';
+  end;
+
 begin
   FEditor := TSynEdit.Create(nil);
   FEditor.Align := alClient;
+  FEditor.SetDefaultKeystrokes;
+  FEditor.Highlighter := TPGMHighLighter.Create(FEditor);
+
+  PopupMenu := TPopupMenu.Create(FEditor);
+  PopupMenu.Items.Add(CreateActionAndMenuItem('Copy', @NoneExecuteAction, 0));
+  PopupMenu.Items.Add(CreateActionAndMenuItem('Cut', @NoneExecuteAction, 0));
+  PopupMenu.Items.Add(CreateActionAndMenuItem('Paste', @NoneExecuteAction, 0));
+  PopupMenu.Items.Add(CreateDivider());
+  PopupMenu.Items.Add(CreateActionAndMenuItem('Run Selected', @NoneExecuteAction, 0));
+  PopupMenu.Items.Add(CreateActionAndMenuItem('Run All', @NoneExecuteAction, 0));
+
+  FEditor.PopupMenu := PopupMenu;
 
   FFileName := '';
 end;
 
 destructor TEditorPage.Destroy;
 begin
+  FEditor.Free;
   inherited Destroy;
 end;
 
