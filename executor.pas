ab23fad5dc61b6905110779e05a871f367c6ce8c
charlesknightsbridge <charlesknightsbridge@gmail.com>
2021-05-06 22:08:25 +0000

    fixed: for macOS, set "output format" can only take the value "TEXT", since component HTMLVIEWER does not work

--- executor.pas
+++ executor.pas
@@ -1870,7 +1870,9 @@ begin
 
   // OUTPUT VIEWER
   SOpt := TSetOption.Create('TEXT', rtString);
+  {$IFNDEF LCLCOCOA}
   SOpt.LegalValues.Add('HTML');
+  {$ENDIF}
   SOpt.LegalValues.Add('TEXT');
   {$IFDEF EPI_CHROMIUM_HTML}
   SOpt.LegalValues.Add('OLDHTML');
