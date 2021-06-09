eddea9f039fd4c75eff6991f80505e84cf55ebe5
charlesknightsbridge <charlesknightsbridge@gmail.com>
2021-06-09 19:27:27 +0000

    fixed: html now legal option for output format with cocoa; still need to work on getting css to work

--- executor.pas
+++ executor.pas
@@ -1870,9 +1870,7 @@ begin
 
   // OUTPUT VIEWER
   SOpt := TSetOption.Create('TEXT', rtString);
-  {$IFNDEF LCLCOCOA}
   SOpt.LegalValues.Add('HTML');
-  {$ENDIF}
   SOpt.LegalValues.Add('TEXT');
   {$IFDEF EPI_CHROMIUM_HTML}
   SOpt.LegalValues.Add('OLDHTML');
