3cce366bafe8ef3227060013eb0d54506c9d111b
Jamie <charlesknightsbridge@gmail.com>
2023-01-14 13:36:47 -0500

    bypass error condition "DrawData leak"
    An ugly fix for error after using Pareto command

--- src/main.pas
+++ src/main.pas
@@ -1621,16 +1621,20 @@ end;
 
 procedure TMainForm.MainExceptionHandler(Sender: TObject; E: Exception);
 begin
-  ShowMessage(
-    'An unhandled error occurred in the program!' + LineEnding +
-    LineEnding +
-    'Please verify that the error can be reproduced.' + LineEnding +
-    'Report to the: EpiData-list (mail.....)' + LineEnding +
-    '               and mail to info@epidata.dk' + LineEnding +
-    LineEnding +
-    'Include most recent 5-10 lines of history in your report.'
-  );
-
+  case E.Message.ToLower of
+    'drawdata leak':
+      ; // ugly trap to Draw Data leak error condition caused by persistent axis transformations
+  else
+      ShowMessage(
+        'An unhandled error occurred in the program!' + LineEnding +
+        LineEnding +
+        'Please verify that the error can be reproduced.' + LineEnding +
+        'Report to the: EpiData-list (mail.....)' + LineEnding +
+        '               and mail to info@epidata.dk' + LineEnding +
+        LineEnding +
+        'Include most recent 5-10 lines of history in your report.'
+      );
+  end;
   Screen.Cursor := crDefault;
 end;
 
