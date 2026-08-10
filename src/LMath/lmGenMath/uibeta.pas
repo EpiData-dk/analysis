4517578da9e271370b493ef526b970f8079d73f1
Jamie <charlesknightsbridge@gmail.com>
2026-08-10 09:00:55 -0400

    fix LMATH units using GOTO
    add -Sg switch

--- src/LMath/lmGenMath/uibeta.pas
+++ src/LMath/lmGenMath/uibeta.pas
@@ -2,7 +2,7 @@
   Incomplete Beta function.
   Translated from C code in Cephes library (http://www.moshier.net)
   ****************************************************************** }
-{$mode objfpc}{$H+}
+{$mode objfpc}{$H+}{-Sg}
 
 unit uibeta;
 
